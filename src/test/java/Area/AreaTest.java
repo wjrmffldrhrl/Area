b19c3781de91a6434de2546e703ffc8dba0f29ee
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-10-25 17:33:33 +0900

    add test version 0.0.3

--- src/test/java/Area/AreaTest.java
+++ src/test/java/Area/AreaTest.java
@@ -21,4 +21,39 @@ public class AreaTest {
     public void sameIndexTest() {
         new Area(4 , 4);
     }
+
+    @Test
+    public void overlapTest() {
+        Area leftArea = new Area(1,5);
+        Area centerArea = new Area(2,7);
+        Area smallCenterArea = new Area(3,6);
+        Area rightArea = new Area(6,9);
+        Area otherArea = new Area(9,13);
+
+        Assert.assertTrue(leftArea.isOverlap(centerArea));
+        Assert.assertTrue(rightArea.isOverlap(centerArea));
+        Assert.assertTrue(centerArea.isOverlap(smallCenterArea));
+        Assert.assertTrue(smallCenterArea.isOverlap(centerArea));
+        Assert.assertFalse(rightArea.isOverlap(otherArea));
+
+    }
+
+    @Test
+    public void equalsTest() {
+        Area a1 = new Area(1,3);
+        Area a2 = new Area(1,3);
+
+        Area a3 = new Area(1,2);
+
+        Assert.assertTrue(a1.equals(a2));
+        Assert.assertFalse(a1.equals(a3));
+    }
+
+    @Test
+    public void cloneTest() {
+        Area originalArea = new Area(1,3);
+        Area cloneArea = originalArea.clone();
+
+        Assert.assertTrue(originalArea.equals(cloneArea));
+    }
 }
